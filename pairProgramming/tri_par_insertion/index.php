7dca07d36f85cd5d1ffdb9967cfbef5f24835a3d
solenehuault <solene.huault@gmail.com>
2016-08-26 13:58:58 +0200

    tri par insertion done

--- pairProgramming/tri_par_insertion/index.php
+++ pairProgramming/tri_par_insertion/index.php
@@ -1,42 +1,20 @@
 <?php
 	$tableau = array("nuage", "mot", "word", "really big word", "little word", "little", "big",  "extraordinaire", "chocolat", "petit", "grand");
-	echo "Tableau d'entrée :";
+	echo "Entrée :";
 	print_r($tableau);
 	echo "<br /><br />";
 	$grand = strlen($tableau[0]);
 
 	for ($i = 1 ; $i < count($tableau) ; $i++) {
-		if ($grand > strlen($tableau[$i])) {
-			$mot = $tableau[$i];
-			$tableau[$i] = $tableau[$i - 1];
-			$tableau[$i - 1] = $mot;
-			$autre_grand = strlen($tableau[0]);
-			if ($i > 2) {
-				for ($j = 1 ; $j < $i ; $j++) {
-					if($autre_grand > strlen($tableau[$j])) {
-						$mot = $tableau[$j];
-						$tableau[$j] = $tableau[$j - 1];
-						$tableau[$j - 1] = $mot;
-					}
-					else {
-						$autre_grand = strlen($tableau[$j]);
-					}
-					echo "tableau dans la double boucle :";
-					print_r($tableau);
-					echo"<br />";
-				}
-			}
-			echo "<br />";
+		$j = $i;
+		while(strlen($tableau[$j]) < strlen($tableau[$j-1])) {
+			$mot = $tableau[$j];
+			$tableau[$j] = $tableau[$j - 1];
+			$tableau[$j - 1] = $mot;
+			$j--;
 		}
-		else {
-			$grand = strlen($tableau[$i]);
-		}
-		echo "tableau dans la boucle";
-		print_r($tableau);
-		echo "<br />";
 	}
-
-	echo "<br /> Tableau de sortie :";
+	echo "<br />Sortie :";
 	print_r($tableau);
 
 ?>
